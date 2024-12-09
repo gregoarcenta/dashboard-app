854c8dfa0e0ef021f4edaca2ddc3e26d764a39ca
Gregory Arcentales <gregoarcenta@gmail.com>
2024-12-09 12:33:17 -0500

    Replace SideMenuComponent with GabSideMenuComponent

--- src/app/features/panel/layouts/panel-layout/panel-layout.component.ts
+++ src/app/features/panel/layouts/panel-layout/panel-layout.component.ts
@@ -1,16 +1,17 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuComponent } from '@/shared/components/side-menu/side-menu.component';
 import { NavbarComponent } from '@/shared/components/navbar/navbar.component';
+import { GabSideMenuComponent, TitleColor } from 'gab-side-menu';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, SideMenuComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, GabSideMenuComponent],
   templateUrl: './panel-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class PanelLayoutComponent {
   protected isAuthenticated = signal(false);
+  TitleColor = TitleColor;
 
   onSignIn() {
     this.isAuthenticated.set(true);
